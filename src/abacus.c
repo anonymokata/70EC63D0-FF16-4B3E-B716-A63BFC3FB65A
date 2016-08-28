bded15aa3bf5f100e81645589cf60d23e724fd12
anonymous <anonymous>
2016-08-28 17:04:33 -0400

    Initialize all of the abacus structure.

--- src/abacus.c
+++ src/abacus.c
@@ -11,9 +11,20 @@ struct Abacus
 
 Abacus *abacus_create(void)
 {
+  int i;
   Abacus *abacus;
   abacus = malloc(sizeof(Abacus));
   strcpy(abacus->symbols, "MDCLXVI");
+  abacus->multi[0]=true;
+  abacus->multi[1]=false;
+  abacus->multi[2]=true;
+  abacus->multi[3]=false;
+  abacus->multi[4]=true;
+  abacus->multi[5]=false;
+  for (i=0;i<6;++i)
+  {
+    abacus->count[i]=0;
+  }
   return abacus;
 }
 
